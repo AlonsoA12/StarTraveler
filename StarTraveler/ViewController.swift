8ba0b5896c3609f2d671b14ccfe4d231a1fcf1c1
ALONSO ALCANTAR <rl236599@rlas-macair-21.rlas-116.org>
2019-02-01 11:19:41 -0600

    Initial Commit

---
+++ StarTraveler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTraveler
+//
+//  Created by ALONSO ALCANTAR on 2/1/19.
+//  Copyright © 2019 ALONSO ALCANTAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
